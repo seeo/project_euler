0bb9cb7c28c33ac857334a3f13e7c68d68c3da50
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-07-05 11:17:42 +0800

    initial commit with template for problem 1: multiple of 3 and 5

---
+++ 1_mult_of_3_and_5.js
@@ -0,0 +1,13 @@
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Hence, find the sum of all the multiples of 3 or 5 below 1000. */
+
+//first we get comfortable with getting numbers 3,5,6,9 from within and inclu. 0 to 10.
+//Then sum them up, check if result == 23
+
+let limit = 10;
+
+for(let i=0; i<name; i++){
+
+}
