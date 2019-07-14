0b857dcf41b4b5a957f5797207d8522023937a3a
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-07-14 22:21:52 +0800

    still working on the largest palindrome product

--- 4_largest_palindrome_product.js
+++ 4_largest_palindrome_product.js
@@ -8,8 +8,7 @@ Find the largest palindrome made from the product of two 3 - digit numbers.
 //we need the product of TWO 3-digit numbers; that has a value that is the same when read right to left.;
 //the product of 999 x 999 = 998,001;
 //the product of 100 x 100 = 10,000;
-
-
+//so we know that the product: 10,000 < product < 998,001;
 
 //we created a helper function here to check if a product returned is a palindrome...
 module.exports.checkIfProductIsPalindrome = (product) => {
@@ -38,14 +37,51 @@ module.exports.checkIfProductIsPalindrome = (product) => {
     let joinedReverseOutput = reverseOutput.join("");
     let toNumberJoinedReversedOutput = parseInt(joinedReverseOutput);
 
-    if(product === toNumberJoinedReversedOutput){
+    if (product === toNumberJoinedReversedOutput) {
         console.log("YES!!!!", product);
-        return true;
-    }else{
+        return product;
+    } else {
         console.log("Nooo...: ", toNumberJoinedReversedOutput);
-        return false;
     }
 }
+
+//now we create a function to create possible products that may be a palindrome...
+
+module.exports.getGreatestPalindrome =() => {
+    let greatestPalindrome = 0;
+    let greaterPalindrome = 0;
+    let test = 0;
+    let x = 100;
+    let y = 100;
+
+    for (x = 100; x < 1000; ++x){
+        test = this.checkIfProductIsPalindrome(x*y);
+        if (greatestPalindrome < this.checkIfProductIsPalindrome(x * y)) {
+            greatestPalindrome = this.checkIfProductIsPalindrome(x * y);
+        } else {
+            greaterPalindrome = this.checkIfProductIsPalindrome(x * y);
+            greatestPalindrome = this.checkIfProductIsPalindrome((x + 1) * y);
+            if (greatestPalindrome > greaterPalindrome) {
+                greaterPalindrome = greatestPalindrome;
+            }
+        }
+
+
+        for (y = 100; y< 1000; ++y){
+            greaterPalindrome = this.checkIfProductIsPalindrome(x * y);
+            greatestPalindrome = this.checkIfProductIsPalindrome(x * (y+1));
+            if (greatestPalindrome > greaterPalindrome){
+                greaterPalindrome = greatestPalindrome;
+            }
+        }
+
+
+    }
+    console.log('greatest palindrome is: ', greatestPalindrome);
+    return greatestPalindrome;
+}
+
+
 
 
 
